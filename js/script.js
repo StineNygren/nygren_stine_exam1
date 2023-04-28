bd1cf41e0a1b26cf200f6339e10617dd7ad471b1
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-04-28 19:51:05 +0200

    coffee icon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const navigationSection = document.querySelector(".navigation-list");
 const toggleButton = document.querySelector(".toggle-butten");
+const coffeeIcon = document.querySelector(".coffee-icon");
 
 toggleButton.addEventListener("click", () => {
   navigationSection.classList.toggle("active");
+  coffeeIcon.classList.toggle("active");
 });
 
 function myFunction(x) {
