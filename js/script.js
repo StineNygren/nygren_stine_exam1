c065ab45e96dab4b001b64ae93f6e5b36754a33c
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-04-30 17:39:00 +0200

    started on hero section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const navigationSection = document.querySelector(".navigation-list");
 const toggleButton = document.querySelector(".toggle-butten");
 const coffeeIcon = document.querySelector(".coffee-icon");
+const imgHero = document.querySelector(".img-hero-section");
 
 toggleButton.addEventListener("click", () => {
   navigationSection.classList.toggle("active");
   coffeeIcon.classList.toggle("active");
+  imgHero.classList.toggle("active");
 });
 
 function myFunction(x) {
