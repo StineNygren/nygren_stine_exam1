96011226cc3940bee40f14f2c5afeb510116354d
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-05-10 09:44:45 +0200

    made const on contact page

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+const name = document.querySelector("#name");
+const subject = document.querySelector("#subject");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
